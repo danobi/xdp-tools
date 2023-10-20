danobi/xdp-tools

commit 13eda42db28535b08a1634f04ff411342e667d50

xdp-bench: Fix online cpu processing

xdp-bench/xdp_redirect_cpumap.c
@@ -440,13 +440,9 @@ int do_redirect_cpumap(const void *cfg, __unused const char *pin_root_path)
                         pr_warn("Failed to get online CPUs, err=%d\n", err);
                         ret = EXIT_FAIL;
                         goto end;
-                } else if (online_cpus_sz != n_cpus) {
-                        pr_warn("Total CPU count changed\n");
-                        ret = EXIT_FAIL;
-                        goto end;
                 }
 
-		for (j = 0; j < n_cpus; j++) {
+		for (j = 0; j < min(n_cpus, online_cpus_sz); j++) {
                         if (!online_cpus[j])
                                 continue;
 			if (create_cpu_entry(j, &value, j, true) < 0) {

commit 54b04be7c1250f1f661e3576f445cee0827307b9

xdp-bench: Fix cpus_available entries
Online CPUs are not necessarily contiguous and 0 indexed.

Fixes: 13eda42 ("xdp-bench: Fix online cpu processing")

xdp-bench/xdp_redirect_cpumap.c
@@ -275,7 +275,7 @@ int do_redirect_cpumap(const void *cfg, __unused const char *pin_root_path)
 	struct bpf_cpumap_val value;
 	__u32 infosz = sizeof(info);
 	int ret = EXIT_FAIL_OPTION;
-        int n_online_cpus = 0;
+        __u32 n_online_cpus = 0;
         int online_cpus_sz;
         bool *online_cpus;
 	int n_cpus, fd;
@@ -448,7 +448,7 @@ int do_redirect_cpumap(const void *cfg, __unused const char *pin_root_path)
 		for (j = 0; j < min(n_cpus, online_cpus_sz); j++) {
                         if (!online_cpus[j])
                                 continue;
-			if (create_cpu_entry(j, &value, j, true) < 0) {
+			if (create_cpu_entry(j, &value, n_online_cpus, true) < 0) {
 				pr_warn("Cannot proceed, exiting\n");
 				ret = EXIT_FAIL;
 				goto end_detach;
@@ -457,7 +457,7 @@ int do_redirect_cpumap(const void *cfg, __unused const char *pin_root_path)
 		}
 	}
 
-	pr_debug("AA %s %d Max CPUS %d available CPUS %d%s\n", __func__, __LINE__, n_online_cpus, n_cpus,
+	pr_debug("AA %s %d Max CPUS %u available CPUS %d%s\n", __func__, __LINE__, n_online_cpus, n_cpus,
 			opt->cpus_all ? " use all cpus" : "");
 
 	ret = sample_run(opt->interval, opt->stress_mode ? stress_cpumap : NULL, &value);